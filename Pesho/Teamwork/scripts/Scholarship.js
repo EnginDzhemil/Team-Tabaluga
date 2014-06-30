73f73819b4769b3e5826f3145e590df4c10e2243
dj2616 <thengin@abv.bg>
2014-06-30 23:26:45 +0300

    push files

---
+++ Pesho/Teamwork/scripts/Scholarship.js
@@ -0,0 +1,28 @@
+﻿var count = 200000000;
+
+function rollDice() {
+    count++;
+    document.getElementById("water").style.visibility = "hidden";
+    document.getElementById("win").style.visibility = "hidden";
+
+    var die1 = document.getElementById("die1");
+    var die2 = document.getElementById("die2");
+    var status = document.getElementById("status");
+    var student = document.getElementById("student");
+
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    var d2 = Math.floor(Math.random() * 6) + 1;
+    var diceTotal = d1 + d2;
+    die1.innerHTML = d1;
+    die2.innerHTML = d2;
+    status.innerHTML = "I have " + diceTotal + "!"
+    student.innerHTML = count;
+    if (d1 == d2) {
+        status.innerHTML += " DOUBLES! YEAH! You got Free Education!!!"
+        document.getElementById("win").style.visibility = "visible";
+    }
+    else {
+        document.getElementById("water").style.visibility = "visible";
+        status.innerHTML = "I don't have Double! I Have " + diceTotal + "! DAMN!!!"
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Team-Tabaluga
+=============
+
+SoftUni Teamwork
+
+Бавно зарежда страницата, 
+(failed)
+net::ERR_FILE_NOT_FOUND
+някакъв файл търси, може би е заради това.
