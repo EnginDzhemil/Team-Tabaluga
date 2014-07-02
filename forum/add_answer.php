a45a68c01db94a9aeb253675686f10bff2ac7223
bsdemon <eon@abv.bg>
2014-07-03 01:22:37 +0300

    Update add_answer.php

--- forum/add_answer.php
+++ forum/add_answer.php
@@ -1,24 +1,24 @@
 <?php
 
-$host="localhost"; // Host name 
-$username=""; // Mysql username 
-$password=""; // Mysql password 
-$db_name="test"; // Database name 
-$tbl_name="forum_answer"; // Table name 
+$host="localhost"; 
+$username="tabaluga"; 
+$password="tabaluga1"; 
+$db_name="tabaluga-forum"; 
+$tbl_name="forum_answer";  
 
-// Connect to server and select databse.
+// Connect 
 mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
 mysql_select_db("$db_name")or die("cannot select DB");
 
-// Get value of id that sent from hidden field 
+// getId
 $id=$_POST['id'];
 
-// Find highest answer number. 
+// last answer 
 $sql="SELECT MAX(a_id) AS Maxa_id FROM $tbl_name WHERE question_id='$id'";
 $result=mysql_query($sql);
 $rows=mysql_fetch_array($result);
 
-// add + 1 to highest answer number and keep it in variable name "$Max_id". if there no answer yet set it = 1 
+//maxID+1
 if ($rows) {
 $Max_id = $rows['Maxa_id']+1;
 }
@@ -26,14 +26,14 @@ else {
 $Max_id = 1;
 }
 
-// get values that sent from form 
+// post data 
 $a_name=$_POST['a_name'];
 $a_email=$_POST['a_email'];
 $a_answer=$_POST['a_answer']; 
 
-$datetime=date("d/m/y H:i:s"); // create date and time
+$datetime=date("d/m/y H:i:s"); // create Date&Time
 
-// Insert answer 
+// answer 
 $sql2="INSERT INTO $tbl_name(question_id, a_id, a_name, a_email, a_answer, a_datetime)VALUES('$id', '$Max_id', '$a_name', '$a_email', '$a_answer', '$datetime')";
 $result2=mysql_query($sql2);
 
@@ -41,7 +41,7 @@ if($result2){
 echo "Successful<BR>";
 echo "<a href='view_topic.php?id=".$id."'>View your answer</a>";
 
-// If added new answer, add value +1 in reply column 
+// +1 in reply column 
 $tbl_name2="forum_question";
 $sql3="UPDATE $tbl_name2 SET reply='$Max_id' WHERE id='$id'";
 $result3=mysql_query($sql3);
