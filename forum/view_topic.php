3936d73234d40a4e1767fde99a81619c67290de7
bsdemon <eon@abv.bg>
2014-07-03 01:22:09 +0300

    Update view_topic.php

--- forum/view_topic.php
+++ forum/view_topic.php
@@ -1,38 +1,113 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabaluga University</title>
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/Courses.css">
+	<link rel="stylesheet" type="text/css" href="../css/forum.css">
+	<style>
+	    
+	 </style>
+</head>
+<body>
 
-$host="localhost"; // Host name 
-$username=""; // Mysql username 
-$password=""; // Mysql password 
-$db_name="test"; // Database name 
-$tbl_name="forum_question"; // Table name 
+<!-- Site Header and Navigation -->
+<nav class="navbar navbar-inverse navbar-top" role="navigation">
+	<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			</button>
+			<div class="row">
+				<a class="navbar-brand" href="#">  <img style="max-width:100px; margin-top: -13px;max-height:100px;" src="../images/tabaluga-1.png"  alt="logo">Tabaluga University</a>
+			</div>
+			</div>
 
-// Connect to server and select databse.
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<div class="row">
+					<form class="navbar-form navbar-right" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Търсене">
+					</div>
+					<button class="btn btn-searchBtn">Търси</button>
+					</form>
+				</div>
+				<div class="row">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="index.html">Начало</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">За университета <span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="Teachers.html">Преподаватели</a></li>
+								<li><a href="LifeInSoftUni.html">Живота в SoftUni</a></li>
+								<li><a href="Scholarship.html">Стипенидия</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Професии <span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="ninja.html">Програмист-нинджа</a></li>
+								<li><a href="god.html">Programming God</a></li>
+								<li><a href="strong.html">Силен програмист</a></li>
+								<li><a href="bad.html">Некадърен програмист</a></li>
+							</ul>
+						</li>
+						<li class="active"><a href="Courses.html">Курсове</a></li>
+						<li><a href="http://iliailiev.net/forum/main_forum.php">Форум</a></li>
+						<li><a href="contacts.html">Контакти</a></li>
+					</ul>
+				</div>
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</div>
+</nav>
+
+<div class="jumbotrona">
+	<div class="container">
+		
+		<?php
+
+$host="localhost"; 
+$username="tabaluga"; 
+$password="tabaluga1"; 
+$db_name="tabaluga-forum"; 
+$tbl_name="forum_question";  
+
+// Connect 
 mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
 mysql_select_db("$db_name")or die("cannot select DB");
 
-// get value of id that sent from address bar 
+// getID 
 $id=$_GET['id'];
 $sql="SELECT * FROM $tbl_name WHERE id='$id'";
 $result=mysql_query($sql);
 $rows=mysql_fetch_array($result);
 ?>
 
-<table width="400" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<table class="viewtable">
   <tr>
     <td>
-      <table width="100%" border="0" cellpadding="3" cellspacing="1" bordercolor="1" bgcolor="#FFFFFF">
+      <table class="view-ta-table">
         <tr>
-          <td bgcolor="#F8F7F1"><strong><? echo $rows['topic']; ?></strong></td>
+          <td class="bgc topic"><strong><? echo $rows['topic']; ?></strong></td>
         </tr>
         <tr>
-          <td bgcolor="#F8F7F1"><? echo $rows['detail']; ?></td>
+          <td class="bgc post"><? echo $rows['detail']; ?></td>
         </tr>
         <tr>
-          <td bgcolor="#F8F7F1"><strong>By :</strong> <? echo $rows['name']; ?> <strong>Email : </strong><? echo $rows['email'];?></td>
+          <td class="bgc topic"><strong>By :</strong> <? echo $rows['name']; ?> <strong>Email : </strong><? echo $rows['email'];?></td>
         </tr>
     
         <tr>
-          <td bgcolor="#F8F7F1"><strong>Date/time : </strong><? echo $rows['datetime']; ?></td>
+          <td class="bgc topic"><strong>Date/time : </strong><? echo $rows['datetime']; ?></td>
         </tr>
       </table>
     </td>
@@ -42,43 +117,44 @@ $rows=mysql_fetch_array($result);
 
 <?php
 
-$tbl_name2="forum_answer"; // Switch to table "forum_answer"
+$tbl_name2="forum_answer"; //table "forum_answer"
 $sql2="SELECT * FROM $tbl_name2 WHERE question_id='$id'";
 $result2=mysql_query($sql2);
 while($rows=mysql_fetch_array($result2)){
 ?>
 
-<table width="400" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<table class="viewtable">
+ <tr>
+    <td><table class="view-ta-table">
   <tr>
-    <td><table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-  <tr>
-    <td bgcolor="#F8F7F1"><strong>ID</strong></td>
-    <td bgcolor="#F8F7F1">:</td>
-    <td bgcolor="#F8F7F1"><? echo $rows['a_id']; ?></td>
+    <td class="bgc"><strong>ID</strong></td>
+    <td class="bgc">:</td>
+    <td class="bgc"><? echo $rows['a_id']; ?></td>
   </tr>
   <tr>
-    <td width="18%" bgcolor="#F8F7F1"><strong>Name</strong></td>
-    <td width="5%" bgcolor="#F8F7F1">:</td>
-    <td width="77%" bgcolor="#F8F7F1"><? echo $rows['a_name']; ?></td>
+    <td width="10%" class="bgc"><strong>Name</strong></td>
+    <td width="3%" class="bgc">:</td>
+    <td width="77%" class="bgc"><? echo $rows['a_name']; ?></td>
   </tr>
   <tr>
-    <td bgcolor="#F8F7F1"><strong>Email</strong></td>
-    <td bgcolor="#F8F7F1">:</td>
-    <td bgcolor="#F8F7F1"><? echo $rows['a_email']; ?></td>
+    <td class="bgc"><strong>Email</strong></td>
+    <td class="bgc">:</td>
+    <td class="bgc"><? echo $rows['a_email']; ?></td>
   </tr>
   <tr>
-    <td bgcolor="#F8F7F1"><strong>Answer</strong></td>
-    <td bgcolor="#F8F7F1">:</td>
-    <td bgcolor="#F8F7F1"><? echo $rows['a_answer']; ?></td>
+    <td class="bgc"><strong>Answer</strong></td>
+    <td class="bgc">:</td>
+    <td class="bgc"><? echo $rows['a_answer']; ?></td>
   </tr>
   <tr>
-    <td bgcolor="#F8F7F1"><strong>Date/Time</strong></td>
-    <td bgcolor="#F8F7F1">:</td>
-    <td bgcolor="#F8F7F1"><? echo $rows['a_datetime']; ?></td>
+    <td class="bgc"><strong>Date/Time</strong></td>
+    <td class="bgc">:</td>
+    <td class="bgc"><? echo $rows['a_datetime']; ?></td>
   </tr>
 </table></td>
 </tr>
-</table><br>
+</table>
+<br>
  
 <?php
 }
@@ -88,14 +164,14 @@ $result3=mysql_query($sql3);
 $rows=mysql_fetch_array($result3);
 $view=$rows['view'];
  
-// if have no counter value set counter = 1
+// if no => counter = 1
 if(empty($view)){
 $view=1;
 $sql4="INSERT INTO $tbl_name(view) VALUES('$view') WHERE id='$id'";
 $result4=mysql_query($sql4);
 }
  
-// count more value
+// count 
 $addview=$view+1;
 $sql5="update $tbl_name set view='$addview' WHERE id='$id'";
 $result5=mysql_query($sql5);
@@ -103,15 +179,15 @@ mysql_close();
 ?>
 
 <BR />
-<table width="400" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<table >
   <tr>
     <form name="form1" method="post" action="add_answer.php">
     <td>
-      <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+      <table class="viewtable-a">
         <tr>
         <td width="18%"><strong>Name</strong></td>
-          <td width="3%">:</td>
-          <td width="79%"><input name="a_name" type="text" id="a_name" size="45"></td>
+          <td width="5%">:</td>
+          <td width="70%"><input name="a_name" type="text" id="a_name" size="45"></td>
           </tr>
         <tr>
           <td><strong>Email</strong></td>
@@ -133,3 +209,22 @@ mysql_close();
     </form>
   </tr>
 </table>
+
+		
+	</div>
+</div>
+<nav class="navbar navbar-inverse navbar-fixed-bottom">
+	<div class="container">
+		<div class="navbar navbar-right">
+			<button class="btn btn-default">Petar Genov</button> 
+			<button class="btn btn-default">Engin Dzhemil</button> 
+			<button class="btn btn-default">Iliya Iliev</button> 
+			<button class="btn btn-default">Desislav Delchev</button> 
+		</div>
+	</div>
+</nav>
+
+<script src="js/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
